21185162ae98e7f933a8e5197f089fae77966de5
joseluna26 <joseluis.lunamx@gmail.com>
2023-12-01 15:02:03 -0600

    Se comentó una parte del LoginRepository para que no marcara error.

---
+++ src/db/repositorys/EmpleadoRepository.java
@@ -0,0 +1,5 @@
+package db.repositorys;
+
+public class EmpleadoRepository {
+    
+}
---
+++ src/repositorys/LoginRepository.java
@@ -0,0 +1,10 @@
+package repositorys;
+
+public class LoginRepository {
+    
+// private boolean iniciarSesion(String usuario, String contrasenia){
+    
+//     return ;
+// }
+
+}
